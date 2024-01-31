66ab02c0e83a511338ec8f7b463fdf31f129be5c
Julian <rodriguezjuliann@outlook.com>
2024-01-31 00:35:19 -0300

    Entrega 1

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
 class ProductManager {
     constructor() {
-      this.products = [];
-      this.productIdCounter = 1;
+      this.products = []
+      this.productIdCounter = 1
     }
   
     addProduct(title, description, price, thumbnail, code, stock) {
       if (![title, description, price, thumbnail, code, stock].every(Boolean)) {
-        console.error("Todos los campos son obligatorios.");
-        return;
+        console.error("Todos los campos son obligatorios.")
+        return
       }
   
       if (this.products.some(product => product.code === code)) {
-        console.error("El código ya está en uso. Por favor, elija otro.");
-        return;
+        console.error("El código ya está en uso. Por favor, elija otro.")
+        return
       }
   
       const newProduct = {
@@ -25,21 +25,32 @@ class ProductManager {
         stock
       };
   
-      this.products.push(newProduct);
-      console.log(`Producto agregado correctamente. ID: ${newProduct.id}`);
+      this.products.push(newProduct)
+      console.log(`Producto agregado correctamente. ID: ${newProduct.id}`)
     }
   
     getProducts() {
-      return this.products.slice();
+      return this.products.slice()
     }
   
     getProductById(id) {
-      const product = this.products.find(product => product.id === id);
+      const product = this.products.find(product => product.id === id)
   
       if (product) {
-        return product;
+        return product
       } else {
-        console.error("Producto no encontrado. ID no válido.");
+        console.error("Producto no encontrado. ID no válido.")
       }
     }
   }
+
+const productManager = new ProductManager()
+const initialProducts = productManager.getProducts()
+console.log("Productos iniciales:", initialProducts)
+
+const productsAfterAdd = productManager.getProducts()
+console.log("Productos después de agregar:", productsAfterAdd)
+
+const productIdToSearch = 1
+const foundProduct = productManager.getProductById(productIdToSearch)
+console.log(`Producto con ID ${productIdToSearch}:`, foundProduct)
